f6ea54b4bfd174119048e8eb297f3b93e6038cd8
Yomguithereal <guillaumeplique@gmail.com>
2016-07-27 17:05:56 +0200

    Adding reference alphabet

--- src/PhonogramCode.js
+++ src/PhonogramCode.js
@@ -1,3 +1,4 @@
+/* eslint no-cond-assign: 0 */
 /**
  * Phonogram Code Abstract
  * ========================
--- test/french/poetic.js
+++ test/french/poetic.js
@@ -119,7 +119,7 @@ export default {
 
     ['xylophone', 'gzilofon'],
 
-    ['yanick', 'janik'],
+    ['yannick', 'janik'],
     ['yeux', 'jø']
   ]
 };
