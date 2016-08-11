c42bcdc6a308f594373b3034b89e19a8c10ded17
Yomguithereal <guillaumeplique@gmail.com>
2016-08-11 14:22:03 +0200

    Adding missing heuristics to git

---
+++ src/french/heuristics.js
@@ -0,0 +1,35 @@
+/**
+ * Phonogram French heuristics
+ * ============================
+ *
+ * Heuristics related to the linguistic origin of some French words.
+ */
+
+// NOTE: "varech" is not greek, but you see the point...
+const GREEK_CH = [
+  'arché',
+  'archo',
+  'chalc',
+  'chald',
+  'chalqu',
+  'charism',
+  'chiro(?!u)',
+  'chor(?!izo)',
+  'lichen',
+  'tricho',
+  'stocha',
+  'varech'
+];
+
+const GREEK_CH_REGEX = new RegExp(`(?:${GREEK_CH.join('|')})`);
+
+/**
+ * Function returning whether the given word's "ch" should be pronounced
+ * the greek way.
+ *
+ * @param  {string}  word - The target word.
+ * @return {boolean}
+ */
+export function isGreekCh(word) {
+  return GREEK_CH_REGEX.test(word);
+}
