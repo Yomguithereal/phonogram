829e30cb26286f6f4274ecf0ce8b1cd82a3e2a6b
Yomguithereal <guillaumeplique@gmail.com>
2018-02-26 16:40:08 +0100

    Improving Italian

--- test/italian/poetic.js
+++ test/italian/poetic.js
@@ -7,6 +7,15 @@ import {poetic} from '../../src/italian';
 export default {
   fn: poetic,
   cases: [
+    ['anche', 'anke'],
 
+    ['Donato', 'donato'],
+
+    ['facevo', 'fatʃevo'],
+
+    ['gnoco', 'njoko'],
+
+    ['Marzo', 'martso'],
+    ['Mauri', 'maʊri']
   ]
 };
